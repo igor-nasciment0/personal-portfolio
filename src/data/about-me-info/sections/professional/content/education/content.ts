5724065de60af3e1f410466b309f68477ff9e68f
Igor <nascimentoigor1311@gmail.com>
2024-05-23 19:22:11 -0300

    fix: texto da faculdade

--- src/data/about-me-info/sections/professional/content/education/content.ts
+++ src/data/about-me-info/sections/professional/content/education/content.ts
@@ -6,7 +6,7 @@ const EducationContent: LanguageContent = {
         p1: "Desde o Ensino Fundamental, tenho desenvolvido gosto pela Matemática e pela Lógica. Durante meu ensino médio na E.E. Laerte Ramos de Carvalho, conquistei duas medalhas de bronze nas Olimpíadas Brasileiras de Matemática (1º e 3º anos).",
         p2: "Isto me motivou a buscar aprender a respeito da área de Computação, que sempre me interessou, pesquisando e fazendo cursos online como o CS50, o curso introdutório de Ciência da Computação de Harvard.",
         p3: "No terceiro ano do ensino médio, busquei ampliar meu conhecimento em programação por meio do curso de informática do Instituto Social Nossa Senhora de Fátima. Lá, aprendi a desenvolver sites e APIs, adquirindo conhecimentos fundamentais do desenvolvimento web.",
-        p4: "Atualmente, estou em busca de uma formação acadêmica que consolide e expanda meu conhecimento em desenvolvimento de software e das bases da Ciência da Computação. Para isso, tenho buscado a entrada na faculdade." 
+        p4: "Atualmente, estou em busca de uma formação acadêmica que consolide e expanda meu conhecimento em desenvolvimento de software e das bases da Ciência da Computação. Para tanto, ingressei no curso de Análise e Desenvolvimento de Sistemas na Fatec, com previsão de término para agosto/2027." 
     },
 
     en_us: {
@@ -14,7 +14,7 @@ const EducationContent: LanguageContent = {
         p1: "Since elementary school, I have developed a liking for Mathematics and Logic. During my high school years at E.E. Laerte Ramos de Carvalho, I earned two bronze medals in the Brazilian Mathematics Olympiad (1st and 3rd years)",
         p2: "This motivated me to pursue learning about the field of Computer Science, which has always interested me, by researching and taking online courses such as CS50, Harvard's introductory course in Computer Science.",
         p3: "In the third year of high school, I sought to expand my programming knowledge through the computer course at Instituto Social Nossa Senhora de Fátima. There, I learned to develop websites and APIs, acquiring fundamental knowledge on web development.",
-        p4: "Currently, I am seeking an academic education that consolidates and expands my knowledge in software development and in the Computer Science foundations. To achieve this, I am pursuing entry into university." 
+        p4: "Currently, I am seeking an academic education that consolidates and expands my knowledge in software development and in the Computer Science foundations. To achieve this, I ingressed the Systems Analysis and Development at Fatec, with completion prediction to august/2027." 
     }
 }
 
